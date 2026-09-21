c7284615ee8f68504c388c81d8f544c778091122
Orxan Namazov <orxannamazovld@gmail.com>
2026-09-21 22:22:31 +0400

    security(env): add env.example.js client template

---
+++ env.example.js
@@ -0,0 +1,11 @@
+/**
+ * TDV E-School - Brauzer Ã¼Ã§Ã¼n Lokal MÃ¼hit KonfiqurasiyasÄ±
+ * 
+ * QEYD: Bu faylÄ±n surÉ™tini 'env.js' adÄ± ilÉ™ Ã§Ä±xarÄ±n vÉ™ aÃ§arlarÄ±nÄ±zÄ± daxil edin.
+ * 'env.js' faylÄ± .gitignore daxilindÉ™dir vÉ™ heÃ§ vaxt GitHub-a push edilmir!
+ */
+window.ENV = {
+  FIREBASE_API_KEY: '',
+  FIREBASE_PROJECT_ID: 'tdv-e-school',
+  FIREBASE_AUTH_DOMAIN: 'tdv-e-school.firebaseapp.com'
+};
